d9c31cfd49b15117cd032290702821b9e3494a01
Amit Pandey <154595576+pandeyamit-07@users.noreply.github.com>
2026-01-22 16:52:28 +0530

    Refactor main.jsx to simplify rendering
    Removed BrowserRouter and adjusted rendering structure.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,1 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter basename="/admin">
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-)
+import { createRoot } from 'react-dom/client' import App from './App' import './index.css' createRoot(document.getElementById('root')).render( <React.StrictMode> <App /> </React.StrictMode> )
